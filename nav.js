fb823fba9490596198ddc9ba048e97e032c749a6
mobei <814442542@qq.com>
2020-02-16 10:56:15 +0800

    first commit

---
+++ nav.js
@@ -0,0 +1,8 @@
+function Rendering(){
+    document.getElementById("nav").innerHTML="\t\t<div class=\"nav\">\n" +
+        "\t\t\t<p><a id=\"menu_1\" href=\"menu_1.html\">个人资料</a></p>\n" +
+        "\t\t\t<p><a id=\"menu_2\" href=\"menu_2.html\">生活经历</a></p>\n" +
+        "\t\t\t<p><a id=\"menu_3\" href=\"menu_3.html\">技能情况</a></p>\n" + 
+        "\t\t\t<p><a id=\"menu_3\" href=\"menu_4.html\">联系方式</a></p>\n" +
+        "\t\t</div>";
+ }
